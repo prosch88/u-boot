28e435fc79121d0c3d14de47b43a2041f437ec2f
Svyatoslav Ryhel <clamor95@gmail.com>
2022-08-19 20:34:35 +0300

    spi: tegra20_slink: accept any word length
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- drivers/spi/tegra20_slink.c
+++ drivers/spi/tegra20_slink.c
@@ -209,15 +209,12 @@ static int tegra30_spi_xfer(struct udevice *dev, unsigned int bitlen,
 	const u8 *dout = data_out;
 	u8 *din = data_in;
 	int num_bytes;
-	int ret;
+	int ret = 0;
 
 	debug("%s: slave %u:%u dout %p din %p bitlen %u\n",
 	      __func__, dev_seq(bus), spi_chip_select(dev), dout, din, bitlen);
-	if (bitlen % 8)
-		return -1;
-	num_bytes = bitlen / 8;
 
-	ret = 0;
+	num_bytes = DIV_ROUND_UP(bitlen, 8);
 
 	reg = readl(&regs->status);
 	writel(reg, &regs->status);	/* Clear all SPI events via R/W */
@@ -255,7 +252,7 @@ static int tegra30_spi_xfer(struct udevice *dev, unsigned int bitlen,
 		num_bytes -= bytes;
 
 		clrsetbits_le32(&regs->command, SLINK_CMD_BIT_LENGTH_MASK,
-				bytes * 8 - 1);
+				bitlen - 1);
 		writel(tmpdout, &regs->tx_fifo);
 		setbits_le32(&regs->command, SLINK_CMD_GO);
 
